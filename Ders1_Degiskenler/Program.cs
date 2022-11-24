3b2de2fbaae930c4b20a665fb4af4e968da41639
Lenovo <Lenovo@DESKTOP-AN0O1I9>
2022-11-24 19:19:36 +0300

    Add project files.

---
+++ Ders1_Degiskenler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders1_Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("havale kime gidecek");
+            Console.WriteLine("iban gir");
+            Console.WriteLine("meblağ?");
+
+            string isim = Console.ReadLine();
+
+            Console.WriteLine("isminiz");
+
+            Console.ReadLine();
+
+            //tek satır silmek icin = ctrl+X
+            //geri almak = ctrl+Z
+            //ileri = ctrl + Y
+
+            //yorum satır
+            
+            /* 
+             dgdfgfdg
+            dfgfdgfd
+            gfhfgghj
+            ghjghjg
+             */
+
+        }
+    }
+}
